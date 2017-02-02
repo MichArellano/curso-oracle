e42ae9b4dd04afecf17deff7eb8386f9ac8f2784
MichArellano <mika-69@live.com.mx>
2017-02-01 20:14:53 -0600

    Create tarea.sql

---
+++ tarea.sql
@@ -0,0 +1,9 @@
+inyeccion sql
+Inyección SQL es un método de infiltración de código intruso que se vale de una vulnerabilidad informática presente en una aplicación en el nivel de validación de las entradas para realizar operaciones sobre una base de datos.
+
+El origen de la vulnerabilidad radica en el incorrecto chequeo o filtrado de las variables utilizadas en un programa que contiene, o bien genera, código SQL. Es, de hecho, un error de una clase más general de vulnerabilidades que puede ocurrir en cualquier lenguaje de programación o script que esté embebido dentro de otro.
+
+Se conoce como Inyección SQL, indistintamente, al tipo de vulnerabilidad, al método de infiltración, al hecho de incrustar código SQL intruso y a la porción de código incrustado.
+
+
+Y no se debe usar delete e insert ya que no se deben usar metodos de seguridad para que solo ciertas personas tengan acceso a alguna tabla.
